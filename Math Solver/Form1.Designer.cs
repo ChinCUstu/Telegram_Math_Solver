3900addd14f1ba2ce0d92da349700722da3c4bc2
ChinCUstu <ndulue.chinedu@stu.cu.edu.ng>
2023-05-09 16:59:52 +0100

    - "Add math problem solver feature" - "Fix bug in score display window" - "Update README with usage instructions" - "Refactor code for readability" - "Update dependencies to latest versions" - "Add error handling for web page navigation" - "Improve performance of math problem solver" - "Fix typo in variable name" - "Add error message for failed math problem parsing"

---
+++ Math Solver/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Math_Solver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtset = new System.Windows.Forms.TextBox();
+            this.txtresult = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Timers.Timer();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(2, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(795, 314);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GO!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(92, 326);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(259, 20);
+            this.txtUrl.TabIndex = 2;
+            this.txtUrl.Text = resources.GetString("txtUrl.Text");
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Group URL";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 377);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Current Set:";
+            // 
+            // txtset
+            // 
+            this.txtset.Location = new System.Drawing.Point(91, 373);
+            this.txtset.Name = "txtset";
+            this.txtset.Size = new System.Drawing.Size(260, 20);
+            this.txtset.TabIndex = 5;
+            // 
+            // txtresult
+            // 
+            this.txtresult.Location = new System.Drawing.Point(91, 416);
+            this.txtresult.Name = "txtresult";
+            this.txtresult.Size = new System.Drawing.Size(118, 20);
+            this.txtresult.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 419);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 19);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Result";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.SynchronizingObject = this;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Elapsed);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(363, 372);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(68, 20);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtresult);
+            this.Controls.Add(this.txtset);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button exitButton;
+
+        public System.Timers.Timer timer1;
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtset;
+        private System.Windows.Forms.TextBox txtresult;
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+
+        #endregion
+    }
+}
---
+++ Math Solver/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace Math_Solver
+{
+    public partial class Form1 : Form
+    {
+        private bool _firstLoad;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtUrl.Text != string.Empty)
+            {
+                webBrowser1.Url = new Uri(txtUrl.Text);
+            }
+            else
+            {
+                MessageBox.Show(@"Please enter a valid URL.");
+            }
+        }
+
+        private void timer1_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            txtset.Text = null;
+
+            if (webBrowser1.Document == null ||
+                !int.TryParse(webBrowser1.Document.GetElementById("task_x")?.InnerText, out var num1) ||
+                !int.TryParse(webBrowser1.Document.GetElementById("task_y")?.InnerText, out var num2) ||
+                !int.TryParse(webBrowser1.Document.GetElementById("task_res")?.InnerText, out var res)) return;
+            var op = webBrowser1.Document.GetElementById("task_op")?.InnerText;
+
+            int remain;
+            switch (op)
+            {
+                case "+":
+                    remain = num1 + num2;
+                    break;
+                case "–":
+                    remain = num1 - num2;
+                    break;
+                case "/":
+                    remain = num1 / num2;
+                    break;
+                case "×":
+                    remain = num1 * num2;
+                    break;
+                default:
+                    MessageBox.Show(@"Unknown operator: " + op);
+                    return;
+            }
+
+            txtset.Text = $@"{num1} {op} {num2} = {res}";
+
+            if (remain == res)
+            {
+                txtresult.Text = @"Yes!";
+                webBrowser1.Document.GetElementById("button_correct")?.InvokeMember("click");
+            }
+            else
+            {
+                txtresult.Text = @"No!";
+                webBrowser1.Document.GetElementById("button_wrong")?.InvokeMember("click");
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            switch (_firstLoad)
+            {
+                case false:
+                {
+                    if (webBrowser1.Document != null)
+                        webBrowser1.Document.GetElementById("button_correct")?.InvokeMember("click");
+                    timer1.Enabled = true;
+                    _firstLoad = true;
+                    break;
+                }
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
